00199f3e5fbc340e627cd3d22da320590803b023
jacob <jbs321.work@gmail.com>
2018-06-30 01:44:38 -0700

    add places autocomplete manager

--- src/Managers/Google.php
+++ src/Managers/Google.php
@@ -8,63 +8,76 @@ use GuzzleHttp\Client;
  * Class GooglePlacesManager
  * @Documentation Google Places API - https://developers.google.com/places/web-service/search
  */
-class Google {
+class Google
+{
 
-	const CONFIG_KEY_KEYS   = 'keys';
-	const CONFIG_KEY_GOOGLE = 'google';
+    const CONFIG_KEY_KEYS = 'keys';
+    const CONFIG_KEY_GOOGLE = 'google';
 
-	const KEY_MAPPING = [
-		GoogleMapsManager::class       => 'google_maps',
-		GoogleStaticMapsManager::class => 'google_static_maps',
-		GoogleStreetViewManager::class => 'street_view',
-		GooglePlacesManager::class     => 'google_places',
-	];
+    const KEY_MAPPING = [
+        GoogleMapsManager::class => 'google_maps',
+        GoogleStaticMapsManager::class => 'google_static_maps',
+        GoogleStreetViewManager::class => 'street_view',
+        GooglePlacesManager::class => 'google_places',
+        GooglePlacesAutoCompleteManager::class => 'google_places_autocomplete',
+    ];
 
-	public function maps() : GoogleMapsManager {
-		return $this->getClassManager(GoogleMapsManager::class);
-	}
+    public function maps(): GoogleMapsManager
+    {
+        return $this->getClassManager(GoogleMapsManager::class);
+    }
 
-	public function staticMaps() : GoogleStaticMapsManager {
-		return $this->getClassManager(GoogleStaticMapsManager::class);
-	}
+    public function staticMaps(): GoogleStaticMapsManager
+    {
+        return $this->getClassManager(GoogleStaticMapsManager::class);
+    }
 
-	public function streetView() : GoogleStreetViewManager {
-		return $this->getClassManager(GoogleStreetViewManager::class);
-	}
+    public function streetView(): GoogleStreetViewManager
+    {
+        return $this->getClassManager(GoogleStreetViewManager::class);
+    }
 
-	public function places() : GooglePlacesManager {
-		return $this->getClassManager(GooglePlacesManager::class);
-	}
+    public function places(): GooglePlacesManager
+    {
+        return $this->getClassManager(GooglePlacesManager::class);
+    }
 
-	/**
-	 * Get class ref and return new instance of class
-	 * @param string $className
-	 *
-	 * @return GoogleBaseManager
-	 */
-	public function getClassManager( string $className ) : GoogleBaseManager {
-		/** @var GoogleBaseManager $manager */
-		$manager = new $className();
-		$apiKey  = $this->findConfigKey($className);
-		$manager->setApiKey($apiKey)->setClient(new Client());
-		return $manager;
-	}
-	
-	public function findConfigKey(string $className) {
-		$confKey = self::KEY_MAPPING[$className];
+    public function autoComplete(): GooglePlacesAutoCompleteManager
+    {
+        return $this->getClassManager(GooglePlacesAutoCompleteManager::class);
+    }
 
-		if(!$confKey) {
-			throw new \Exception('Mapping doesn\'t exists');
-		}
+    /**
+     * Get class ref and return new instance of class
+     * @param string $className
+     *
+     * @return GoogleBaseManager
+     */
+    public function getClassManager(string $className): GoogleBaseManager
+    {
+        /** @var GoogleBaseManager $manager */
+        $manager = new $className();
+        $apiKey = $this->findConfigKey($className);
+        $manager->setApiKey($apiKey)->setClient(new Client());
+        return $manager;
+    }
 
-		//Taken from laravel global helper function config()
-		$apiKey         = join('.', [self::CONFIG_KEY_GOOGLE , self::CONFIG_KEY_KEYS, $confKey]);
-		$keyConfigValue = config($apiKey);
+    public function findConfigKey(string $className)
+    {
+        $confKey = self::KEY_MAPPING[$className];
 
-		if(!$keyConfigValue) {
-			throw new \Exception('Missing API key for class: ' . self::class );
-		}
+        if (!$confKey) {
+            throw new \Exception('Mapping doesn\'t exists');
+        }
 
-		return $keyConfigValue;
-	}
+        //Taken from laravel global helper function config()
+        $apiKey = join('.', [self::CONFIG_KEY_GOOGLE, self::CONFIG_KEY_KEYS, $confKey]);
+        $keyConfigValue = config($apiKey);
+
+        if (!$keyConfigValue) {
+            throw new \Exception('Missing API key for class: ' . self::class);
+        }
+
+        return $keyConfigValue;
+    }
 }
---
+++ src/Managers/GooglePlacesAutoCompleteManager.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Google\Managers;
+
+use Exception;
+use Google\Types\GooglePlacesResponse;
+
+/**
+ * Class GooglePlacesManager
+ * @Documentation Google Places Auto-complete API - https://developers.google.com/places/web-service/autocomplete#location_restrict
+ *
+ * @Note Note From Google: You can use Place Autocomplete even without a map. If you do show a map, it must be a Google map.
+ * When you display predictions from the Place Autocomplete service without a map, you  must include the 'Powered by Google' logo.
+ *
+ * No Support for XML currently
+ */
+class GooglePlacesAutoCompleteManager extends GoogleBaseManager
+{
+    protected $apiPath = "https://maps.googleapis.com/maps/api/place/autocomplete";
+
+    /**
+     * @param string $query
+     *
+     * @return GooglePlacesResponse
+     */
+    public function findAddressByQuery(string $query)
+    {
+        $fullQuery = $this->makeApiQuery($query);
+
+        try {
+            $googleResponse = $this->getClient()->get($fullQuery)->getBody();
+        } catch (Exception $te) {
+            abort(500, "Technical Issue please contact System Admin");
+        }
+
+        $data = new GooglePlacesResponse($googleResponse);
+
+        return $data;
+    }
+
+    /**
+     * @param string $query
+     * @param string $returnType
+     *
+     * @return string
+     */
+    public function makeApiQuery($query = "", $returnType = GooglePlacesResponse::KEY__RESPONSE_TYPE_JSON)
+    {
+        $components = ['ca'];
+        $query = join("/", [$this->apiPath, $returnType]) . "?key={$this->apiKey}&input={$query}";
+
+        if(count($components) > 5) {
+            throw new Exception("Currently, you can use components to filter by up to 5 countries. " . count($components) . " given");
+        }
+
+        if (!empty($components)) {
+            $components = array_map(function(string $countryCode) {
+                return "country:{$countryCode}";
+            }, $components);
+            $restrictions = implode("|", $components);
+            return  "$query&$restrictions";
+        }
+    }
+}
